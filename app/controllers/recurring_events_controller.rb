669afc1d25dab7a80249b9dcc0044cc3ebe262d2
pchi17 <pengfei.p.chi@gmail.com>
2016-06-15 21:34:33 -0700

    all controller actions are now working

--- app/controllers/recurring_events_controller.rb
+++ app/controllers/recurring_events_controller.rb
@@ -12,19 +12,29 @@ class RecurringEventsController < ApplicationController
     @recurring_event = RecurringEvent.new(recurring_event_params)
     if @recurring_event.save
       flash[:success] = 'Recurring Event Created!'
-      redirect_to recurring_events_path
+      redirect_to recurring_event_path(@recurring_event)
     else
       render :new
     end
   end
   
   def show
+    @recurring_event = RecurringEvent.find(params[:id])
   end
   
   def edit
+    @recurring_event = RecurringEvent.find(params[:id])
+    render :new
   end
   
   def update
+    @recurring_event = RecurringEvent.find(params[:id])
+    if @recurring_event.update_attributes(recurring_event_params)
+      flash[:success] = 'Recurring Event Updated!'
+      redirect_to recurring_event_path(@recurring_event)
+    else
+      render :edit
+    end
   end
   
   def destroy
